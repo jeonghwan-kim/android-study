47604ec70050dcbab0569f52bef2e21e5ad11c2d
jeonghwan <ej88ej@gmail.com>
2014-10-21 19:30:33 +0900

    hello world, click event handler

---
+++ mobile/src/main/java/com/example/jeonghwan/myapplication4/MyActivity.java
@@ -0,0 +1,47 @@
+package com.example.jeonghwan.myapplication4;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+
+public class MyActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+
+        TextView tvClickMe = (TextView)findViewById(R.id.textViewClickMe);
+        tvClickMe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView tvClickMe = (TextView)view.findViewById(R.id.textViewClickMe);
+                tvClickMe.setText(R.string.clicked);
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.my, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
