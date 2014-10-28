4a20297c8db7c9be0a8550e7219127fb07139ae9
jeonghwan <ej88ej@gmail.com>
2014-10-28 10:16:37 +0900

    Dialog

--- mobile/src/main/java/com/example/jeonghwan/myapplication4/MyActivity.java
+++ mobile/src/main/java/com/example/jeonghwan/myapplication4/MyActivity.java
@@ -1,6 +1,7 @@
 package com.example.jeonghwan.myapplication4;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -8,6 +9,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import android.app.AlertDialog;
 
 
 public class MyActivity extends Activity {
@@ -37,6 +39,29 @@ public class MyActivity extends Activity {
             public void onClick(View v) {
                 Log.d("tag", "Toast clicked");
                 Toast.makeText(v.getContext(), "토스트!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // 다이얼로그 보기 버튼 설정
+        Button btnDialog = (Button)findViewById(R.id.btnDialog);
+        btnDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("tag", "Dialog btn is clicked");
+
+                final String[] cars = new String[] {"320i", "320d", "520d", "730i"};
+
+                // new AlertDialog.Builder(v.getContext()).setIcon(R.drawable.ic_launcher).setTitle("인사")
+                new AlertDialog.Builder(MyActivity.this).setIcon(R.drawable.ic_launcher).setTitle("인사")
+                        // .setMessage("안녕하세요?")
+                        .setItems(cars, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                Log.d("tag", "cars: " + cars[i]);
+                                Toast.makeText(MyActivity.this, "cars: " + cars[i], Toast.LENGTH_SHORT).show();
+                                // 토스트를 띄우려면 컨텍스트를 어떻게 가져올수 있지? 2014/10/28
+                            }
+                        }).setNeutralButton("닫기", null).show();
             }
         });
     }
